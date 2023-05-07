990abe5c6bd677974bba122c90a9b47cc4ef2a01
mnyef <vladgav004@gmail.com>
2023-05-07 14:48:12 +0300

    Express Server and Connect To DB

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-console.log('E-Commerce API');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+// strictQuery
+mongoose.set('strictQuery', true);
+
+// express
+const express = require('express');
+const app = express();
+
+// database
+const connectDB = require('./db/connect');
+
+const port = process.env.PORT || 5000;
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
