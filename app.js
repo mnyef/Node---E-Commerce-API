c61a48988a030472d5affef31229cbdaee5f105a
mnyef <vladgav004@gmail.com>
2023-05-07 14:57:39 +0300

    Basic Routes

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+require('express-async-errors');
+
 const mongoose = require('mongoose');
 
 // strictQuery
@@ -10,6 +12,19 @@ const app = express();
 
 // database
 const connectDB = require('./db/connect');
+
+// middleware
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('e-commerce api');
+});
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 const start = async () => {
