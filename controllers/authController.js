67e03d218777526ee847ecaadd37d2c935927452
mnyef <vladgav004@gmail.com>
2023-05-07 19:57:03 +0300

    JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const { StatusCodes } = require('http-status-codes');
 const CustomError = require('../errors');
+const { createJWT } = require('../utils');
 
 const register = async (req, res) => {
   const { email, name, password } = req.body;
@@ -15,7 +16,10 @@ const register = async (req, res) => {
   const role = isFirstAccount ? 'admin' : 'user';
 
   const user = await User.create({ name, email, password, role });
-  res.status(StatusCodes.CREATED).json({ user });
+  const tokenUser = { name: user.name, userId: user._id, role: user.role };
+  const token = createJWT({ payload: tokenUser });
+
+  res.status(StatusCodes.CREATED).json({ user: tokenUser, token });
 };
 const login = async (req, res) => {
   res.send('login user');
