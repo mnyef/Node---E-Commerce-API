ab6752de99251712c7285db4a88e53f0cdc71dee
mnyef <vladgav004@gmail.com>
2023-05-10 22:25:51 +0300

    Product Routes Structure

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ const connectDB = require('./db/connect');
 // routers
 const authRouter = require('./routes/authRoutes');
 const userRouter = require('./routes/userRoutes');
+const productRouter = require('./routes/productRoutes');
 
 // middleware
 const notFoundMiddleware = require('./middleware/not-found');
@@ -41,6 +42,7 @@ app.get('/api/v1', (req, res) => {
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
+app.use('/api/v1/products', productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
---
+++ controllers/productController.js
@@ -0,0 +1,27 @@
+const createProduct = async (req, res) => {
+  res.send('create product');
+};
+const getAllProducts = async (req, res) => {
+  res.send('get all products');
+};
+const getSingleProduct = async (req, res) => {
+  res.send('get single product');
+};
+const updateProduct = async (req, res) => {
+  res.send('update product');
+};
+const deleteProduct = async (req, res) => {
+  res.send('delete product');
+};
+const uploadImage = async (req, res) => {
+  res.send('upload image');
+};
+
+module.exports = {
+  createProduct,
+  getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
+  uploadImage,
+};
