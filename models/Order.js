ba28f7eab7137ca643fb82df74e88f7d25aad4ea
mnyef <vladgav004@gmail.com>
2023-05-13 22:45:35 +0300

    Create Order Complete

--- models/Order.js
+++ models/Order.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const SingleCartItemSchema = mongoose.Schema({
+const SingleOrderItemSchema = mongoose.Schema({
   name: { type: String, required: true },
   image: { type: String, required: true },
   price: { type: Number, required: true },
@@ -30,7 +30,7 @@ const OrderSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
-    cartItems: [SingleCartItemSchema],
+    orderItems: [SingleOrderItemSchema],
     status: {
       type: String,
       enum: ['pending', 'failed', 'delivered', 'canceled'],
