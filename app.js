2d7d31ddc08344492c115a944c52b33f89b81e51
mnyef <vladgav004@gmail.com>
2023-05-15 18:09:18 +0300

    Few Fixes

--- app.js
+++ app.js
@@ -47,21 +47,11 @@ app.use(cors());
 app.use(xss());
 app.use(mongoSanitize());
 
-app.use(morgan('tiny'));
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 
 app.use(express.static('./public'));
 app.use(fileUpload());
-
-app.get('/', (req, res) => {
-  res.send('e-commerce api');
-});
-app.get('/api/v1', (req, res) => {
-  console.log(req.signedCookies);
-  // console.log(req.cookies);
-  res.send('e-commerce api');
-});
 
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
